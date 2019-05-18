49c994d10330fc6ce7f30cdbfab44ac3824b051f
Malcolm Lynch <milynch1320@gmail.com>
2019-05-18 11:21:36 -0500

    malcolm-lynch

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,6 +7,7 @@ import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import /* You need some sort of reducer */ './reducers';
+//Initialize
 
 const store = createStore(
   () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
